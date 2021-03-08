d17cea71c1833e9db1469ecaae70ad63cb3fcaf2
Mateus Marques <75648672+mateus-mqs@users.noreply.github.com>
2021-03-08 16:36:20 -0300

    tp3 finalized without dinamic mem alocation

--- tps/tp3.c
+++ tps/tp3.c
@@ -11,12 +11,11 @@ struct Pessoa {
 
 struct Pessoa *buscaPessoa(struct Pessoa *pessoaAsc, char nomeProcurado[], int nivel, int imprimeNivel){
     struct Pessoa *buscada;
+    if(strcmp(pessoaAsc->nome,nomeProcurado) == 0){
+        if(imprimeNivel){printf("%i\n",nivel);}
+        return pessoaAsc;
+    }
     for(int i = 0; i < pessoaAsc->numFilhos; i++){
-        *buscada = pessoaAsc->filhos[i];
-        if(strcmp(buscada->nome,nomeProcurado) == 0){ //primeiro verifica se o nome é
-            if(imprimeNivel){printf("%i\n",nivel);}
-            return buscada;
-        }
         buscada = buscaPessoa(&pessoaAsc->filhos[i], nomeProcurado, nivel+1, imprimeNivel);
         if(buscada == NULL){
             continue;
@@ -31,7 +30,11 @@ void cadastraPessoa(struct Pessoa *pessoaAsc, char nome[], int idade, char asc[]
     struct Pessoa *buscada;
     buscada = buscaPessoa(pessoaAsc, asc, 0, 0);
     buscada->numFilhos++;
-    buscada->filhos = realloc(buscada->filhos, buscada->numFilhos * sizeof(struct Pessoa));
+    if(buscada->numFilhos == 1){
+        buscada->filhos = malloc(1*sizeof(struct Pessoa));
+    } else {
+        buscada->filhos = (struct Pessoa *)realloc(buscada->filhos, (buscada->numFilhos)*sizeof(struct Pessoa));
+    }
     buscada->filhos[buscada->numFilhos - 1].idade = idade;
     buscada->filhos[buscada->numFilhos - 1].numFilhos = 0;
     strcpy(buscada->filhos[buscada->numFilhos - 1].nome, nome);
@@ -53,5 +56,5 @@ void main(){
         scanf("%s %s", nome, asc);
         buscaPessoa(buscaPessoa(&pessoaMaster, asc, 0, 0), nome, 0, 1);
     }
-    free(&pessoaMaster);
+    //free(&pessoaMaster);
 }
