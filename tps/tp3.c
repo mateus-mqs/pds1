25831ea37c27bbfb7dea6324dcfbafb5570b044f
Mateus Marques <75648672+mateus-mqs@users.noreply.github.com>
2021-03-08 12:15:51 -0300

    modifying busca function

--- tps/tp3.c
+++ tps/tp3.c
@@ -8,6 +8,7 @@ struct Pessoa {
     int idade, numFilhos;
     struct Pessoa *filhos;
 };
+
 struct Pessoa *buscaPessoa(struct Pessoa *pessoaAsc, char nomeProcurado[], int nivel, int imprimeNivel){
     struct Pessoa *buscada;
     for(int i = 0; i < pessoaAsc->numFilhos; i++){
@@ -28,25 +29,29 @@ struct Pessoa *buscaPessoa(struct Pessoa *pessoaAsc, char nomeProcurado[], int n
 
 void cadastraPessoa(struct Pessoa *pessoaAsc, char nome[], int idade, char asc[]){
     struct Pessoa *buscada;
-    buscada = buscaPessoa(&pessoaAsc, asc, 0, 0);
+    buscada = buscaPessoa(pessoaAsc, asc, 0, 0);
     buscada->numFilhos++;
     buscada->filhos = realloc(buscada->filhos, buscada->numFilhos * sizeof(struct Pessoa));
     buscada->filhos[buscada->numFilhos - 1].idade = idade;
+    buscada->filhos[buscada->numFilhos - 1].numFilhos = 0;
     strcpy(buscada->filhos[buscada->numFilhos - 1].nome, nome);
-
 }
 
 void main(){
-    int n;
+    int n, idade;
+    char nome[50], asc[50];
     scanf("%i",&n);
     struct Pessoa pessoaMaster;
-    scanf("%s %i %s", pessoaMaster.nome, &pessoaMaster.idade);
+    scanf("%s %i %s", pessoaMaster.nome, &pessoaMaster.idade, asc);
     pessoaMaster.numFilhos = 0;
     for(int i = 1; i < n; i++){
-        struct Pessoa *buscada;
-        int idade;
-        char nome[50], asc[50];
         scanf("%s %i %s", nome, &idade, asc);
         cadastraPessoa(&pessoaMaster, nome, idade, asc);
     }
+    scanf("%i",&n);
+    for(int i = 0; i < n; i++){
+        scanf("%s %s", nome, asc);
+        buscaPessoa(buscaPessoa(&pessoaMaster, asc, 0, 0), nome, 0, 1);
+    }
+    free(&pessoaMaster);
 }
